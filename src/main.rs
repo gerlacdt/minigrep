638b8975d938f6f7bc671631cfeacb0d44fd4426
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2022-07-24 12:45:07 +0200

    grep with filename

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use minigrep::Config;
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application Error: {}", e);
+        process::exit(1);
+    }
+}
