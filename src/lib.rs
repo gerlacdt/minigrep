c38194bca341d37865451227b19dc5607f538b88
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2022-08-17 15:40:47 +0200

    support multiple files and stdin

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,69 @@
 use colored::Colorize;
-use regex::RegexBuilder;
+use regex::{Regex, RegexBuilder};
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 use std::{error::Error, io::stdin};
 
 pub fn grep(args: Args) -> Result<(), Box<dyn Error>> {
-    // TODO decide between stdin or filenames
+    let re = create_regex(&args);
     if args.filenames.is_empty() {
-        return from_stdin(args);
-    } else {
-        println!("use filenames");
+        return from_stdin(args, &re);
     }
-    Ok(())
+    from_files(args, &re)
 }
 
-fn from_stdin(args: Args) -> Result<(), Box<dyn Error>> {
-    let lines = stdin().lines();
-    let re = match RegexBuilder::new(&args.query)
+fn create_regex(args: &Args) -> Regex {
+    match RegexBuilder::new(&args.query)
         .case_insensitive(args.insensitive)
         .build()
     {
         Ok(regex) => regex,
         Err(e) => panic!("Error parsing given regexp: {}", e),
-    };
+    }
+}
 
+// use trait for stdin/stdout
+// https://stackoverflow.com/questions/28370126/how-can-i-test-stdin-and-stdout
+fn from_stdin(_args: Args, re: &Regex) -> Result<(), Box<dyn Error>> {
+    let lines = stdin().lock().lines();
     for line in lines {
-        let l = &line.unwrap();
-        if let Some(result) = re.find(l) {
-            let found = result.as_str();
-            let line_to_print = l.replace(found, &found.red().to_string());
-            println!("{}", line_to_print);
+        if let Ok(l) = line {
+            handle_line(&l, &re);
         }
     }
     Ok(())
+}
+
+fn from_files(args: Args, re: &Regex) -> Result<(), Box<dyn Error>> {
+    for filename in args.filenames {
+        if let Ok(lines) = read_lines(filename) {
+            for line in lines {
+                if let Ok(l) = line {
+                    handle_line(&l, &re)
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn handle_line(line: &str, re: &Regex) {
+    if let Some(result) = re.find(line) {
+        let found = result.as_str();
+        let line_to_print = line.replace(found, &found.red().bold().to_string());
+        println!("{}", line_to_print);
+    }
 }
 
 #[derive(clap::Parser, Debug)]
@@ -48,5 +81,19 @@ pub struct Args {
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
+
+    #[test]
+    fn test_filenames() {
+        let args = Args {
+            insensitive: true,
+            query: "foo".to_string(),
+            filenames: vec![
+                "test_files/poem.txt".to_string(),
+                "test_files/foo.txt".to_string(),
+            ],
+        };
+
+        let _ = grep(args);
+    }
 }
